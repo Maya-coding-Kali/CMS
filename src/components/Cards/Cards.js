85ab9bcdcdf6e211e0aff9cf94b8b8e9107f591d
Maya <mayasophiakali@gmail.outlook.com>
2022-07-14 13:12:51 -0700

    add dependency react router dom

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,11 +2,11 @@ import React from "react";
 import simon from "./img/Simon.png";
 import Balloon from "./img/Balloon.png";
 import SmartBrain from "./img/SmartBrain.png";
-
+import "./Cards.css"
 const Cards = () => {
   return (
-    <article className="flex justify-center ma6 flex-wrap ">
-      <div className="ma4 ">
+    <article className="flex justify-center main-space flex-wrap ">
+      <div className="space">
         <a
           className="db center mw5 black link dim "
           title=""     
@@ -15,10 +15,10 @@ const Cards = () => {
           href="http://maya.infinityfreeapp.com/Simon/index.html"
         >
           <h3 className="tc">Simon Game</h3>
-          <img className="db  b--black br4 shadow-1" alt="Simon" src={simon} height="255"></img>
+          <img className="db  b--black br4 shadow-1" alt="Simon" src={simon}></img>
         </a>
       </div>
-      <div className="ma4">
+      <div className="space">
       <h3 className="tc">Balloon Game</h3>
         <a
           className="db center mw5 black link dim "
@@ -27,10 +27,10 @@ const Cards = () => {
             rel="noreferrer"
           href="http://maya.infinityfreeapp.com/Balloons/index.html"
         >
-          <img className="db b--black br4 shadow-1" alt="Balloon" src={Balloon}></img>
+          <img className="db b--black br4 shadow-1" alt="Balloon" src={Balloon} ></img>
         </a>
       </div>
-      <div className="ma4">
+      <div className="space">
       <h3 className="tc">Smart-Brain App</h3>
         <a
           className="db center mw5 black link dim "
