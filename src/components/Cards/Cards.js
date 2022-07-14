94efb362c4f6154666aa19764da36d18bb401fbf
Maya <mayasophiakali@gmail.outlook.com>
2022-07-14 13:21:18 -0700

    add dependency react router dom

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -8,38 +8,38 @@ const Cards = () => {
     <article className="flex justify-center main-space flex-wrap ">
       <div className="space">
         <a
-          className="db center mw5 black link dim "
+          className=""
           title=""     
           target={"_blank"} 
             rel="noreferrer"     
           href="http://maya.infinityfreeapp.com/Simon/index.html"
         >
           <h3 className="tc">Simon Game</h3>
-          <img className="db  b--black br4 shadow-1" alt="Simon" src={simon}></img>
+          <img className="db b--black br4 my-image" alt="Simon" src={simon}></img>
         </a>
       </div>
       <div className="space">
       <h3 className="tc">Balloon Game</h3>
         <a
-          className="db center mw5 black link dim "
+          className=" "
           title=""
           target={"_blank"} 
             rel="noreferrer"
           href="http://maya.infinityfreeapp.com/Balloons/index.html"
         >
-          <img className="db b--black br4 shadow-1" alt="Balloon" src={Balloon} ></img>
+          <img className="db b--black br4 shadow-1 my-image" alt="Balloon" src={Balloon} ></img>
         </a>
       </div>
       <div className="space">
       <h3 className="tc">Smart-Brain App</h3>
         <a
-          className="db center mw5 black link dim "
+          className=""
           title=""
           target={"_blank"} 
             rel="noreferrer"
           href="https://smart-brain987.herokuapp.com/"
         >
-          <img className="db b--black br4 shadow-1" alt="SmtBrn" src={SmartBrain}></img>
+          <img className="db b--black br4 shadow-1 my-image" alt="SmtBrn" src={SmartBrain}></img>
         </a>
       </div>
     </article>
