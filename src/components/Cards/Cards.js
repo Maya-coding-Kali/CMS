796d9383f9cbd0c904c95e44b34e3a39304e2a13
Maya <mayasophiakali@gmail.outlook.com>
2022-08-06 12:37:10 -0700

    run build

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,48 +2,62 @@ import React from "react";
 import simon from "./img/Simon.png";
 import Balloon from "./img/Balloon.png";
 import SmartBrain from "./img/SmartBrain.png";
-import "./Cards.css"
+import "./Cards.css";
 const Cards = () => {
   return (
-    <article className="flex justify-center main-space flex-wrap ">
-    <div className="space">
-      <h3 className="tc">Smart-Brain App</h3>
-        <a
-          className=""
-          title=""
-          target={"_blank"} 
+    <div>
+      <h1 className="tc underline ma5">Projects</h1>
+      <article className="flex justify-center main-space flex-wrap ">
+        <div className="space">
+          <h3 className="tc">Smart-Brain App</h3>
+          <a
+            className=""
+            title=""
+            target={"_blank"}
             rel="noreferrer"
-          href="https://smart-brain987.herokuapp.com/"
-        >
-          <img className="db b--black br4 shadow-1 my-image" alt="SmtBrn" src={SmartBrain}></img>
-        </a>
-      </div>
-      <div className="space">
+            href="https://smart-brain987.herokuapp.com/"
+          >
+            <img
+              className="db b--black br4 shadow-1 my-image"
+              alt="SmtBrn"
+              src={SmartBrain}
+            ></img>
+          </a>
+        </div>
+        <div className="space">
           <h3 className="tc">Simon Game</h3>
-        <a
-          className=""
-          title=""     
-          target={"_blank"} 
-            rel="noreferrer"     
-          href="http://maya.infinityfreeapp.com/Simon/index.html"
-        >
-          <img className="db b--black br4 shadow-1 my-image" alt="Simon" src={simon}></img>
-        </a>
-      </div>
-      <div className="space">
-      <h3 className="tc">Balloon Game</h3>
-        <a
-          className=" "
-          title=""
-          target={"_blank"} 
+          <a
+            className=""
+            title=""
+            target={"_blank"}
             rel="noreferrer"
-          href="http://maya.infinityfreeapp.com/Balloons/index.html"
-        >
-          <img className="db b--black br4 shadow-1 my-image" alt="Balloon" src={Balloon} ></img>
-        </a>
-      </div>
-      
-    </article>
+            href="http://maya.infinityfreeapp.com/Simon/index.html"
+          >
+            <img
+              className="db b--black br4 shadow-1 my-image"
+              alt="Simon"
+              src={simon}
+            ></img>
+          </a>
+        </div>
+        <div className="space">
+          <h3 className="tc">Balloon Game</h3>
+          <a
+            className=" "
+            title=""
+            target={"_blank"}
+            rel="noreferrer"
+            href="http://maya.infinityfreeapp.com/Balloons/index.html"
+          >
+            <img
+              className="db b--black br4 shadow-1 my-image"
+              alt="Balloon"
+              src={Balloon}
+            ></img>
+          </a>
+        </div>
+      </article>
+    </div>
   );
 };
 
